0257d43c10b5ebb577b5f4673f1a879c0526adfb
AymanElbery <ayman.bery@gmail.com>
2020-08-19 09:09:56 +0300

    fix instructor dashboard design

--- resources/views/cp/dashboards/instructor.blade.php
+++ resources/views/cp/dashboards/instructor.blade.php
@@ -8,97 +8,42 @@
                     <div class="card">
                         <div class="widget-header">
                             <div class=" d-flex justify-content-between align-items-center">
-                                <h3 class="widget-title">الدورات الأخيرة</h3>
-                                <a href="{{route('instructor-courses-list', ['type' => 'past'])}}" class="btn btn-all"> المزيد</a>
-                            </div>
-                        </div>
-                        <div class="card-body" style="padding: 0 15px">
-
-                            @foreach($courses as $chunk)
-                                <div class="row odd">
-                                    @foreach($chunk as $course)
-                                        <div class="col-xl-6 col-md-6">
-                                            <div class="proj-progress-card d-flex align-items-center">
-                                                <img src="{{asset($course->image)}}">
-                                                <div>
-                                                    <h6>{{$course->title_ar}}</h6>
-                                                    <div class="row align-items-center justify-content-around">
-                                                        <div class="col">
-                                                            <div class="progress">
-                                                                <div class="progress-bar bg-green" style="width:100%"></div>
-                                                            </div>
-                                                        </div>
-                                                        <div class="col">
-                                                            <h6 class="mb-0 text-success font-weight-bold">100%</h6>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    @endforeach
-                                </div>
-                            @endforeach
-                        </div>
-                    </div>
-                </div>
-                <div class="widget">
-                    <div class="card">
-                        <div class="widget-header">
-                            <div class=" d-flex justify-content-between align-items-center">
-                                <h3 class="widget-title">الدورات التدريبة المفضلة </h3>
-                                <a href="#" class="btn btn-all"> المزيد</a>
+                                <h3 class="widget-title">الدورات التدريبة الحالية </h3>
+                                @if(count($currentCourses) > 0)
+                                <a href="{{route('instructor-courses-list', ['type' => 'current'])}}" class="btn btn-all"> المزيد</a>
+                                @endif
                             </div>
                         </div>
                         <div class="card-body">
                             <div class="row">
+                                @foreach($currentCourses as $course)
                                 <div class="col-md-6 ">
                                     <div class=" course-box">
-                                        <img src="images/img1.jpg" alt="" class="img-thumbnail">
+                                        <img src="{{asset($course->image)}}" alt="" class="img-thumbnail">
                                         <div class="details">
-                                            <h3><a href="#"> اساسيات التعلم عن بعد</a></h3>
+                                            <h3><a href="{{route('instructor-courses-view',['id' => $course->id, 'type' => 'current'])}}"> {{ $course->title_ar }}</a></h3>
                                             <div class="d-flex justify-content-between price">
-                                                <p class="">متبقي 2 يوم </p>
-                                                <h6>14$</h6>
+                                                @if($course->type == 'live')
+                                                <h6>أونلاين</h6>
+                                                @elseif($course->type == 'recorded')
+                                                <h6>دورة مسجلة</h6>
+                                                @elseif($course->type == 'face_to_face')
+                                                <h6>حضور فعلي</h6>
+                                                @else
+                                                <h6>تعليم مدمج</h6>
+                                                @endif
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                                <div class="col-md-6 ">
-                                    <div class=" course-box">
-                                        <img src="images/img2.jpg" alt="" class="img-thumbnail">
-                                        <div class="details">
-                                            <h3><a href="#"> اساسيات التعلم عن بعد</a></h3>
-                                            <div class="d-flex justify-content-between price">
-                                                <p class="">متبقي 2 يوم </p>
-                                                <h6>14$</h6>
-                                            </div>
-                                        </div>
+                                @endforeach
+                                @if(count($currentCourses) == 0)
+                                <div class="col-md-12">
+                                    <div class="alert alert-info" style="text-align: center;">
+                                        <b class="text-center">لا يوجد دورات حالية</b>
                                     </div>
                                 </div>
-                                <div class="col-md-6 ">
-                                    <div class=" course-box">
-                                        <img src="images/img3.jpg" alt="" class="img-thumbnail">
-                                        <div class="details">
-                                            <h3><a href="#"> اساسيات التعلم عن بعد</a></h3>
-                                            <div class="d-flex justify-content-between price">
-                                                <p class="">متبقي 2 يوم </p>
-                                                <h6>14$</h6>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-md-6 ">
-                                    <div class=" course-box">
-                                        <img src="images/law2.jpg" alt="" class="img-thumbnail">
-                                        <div class="details">
-                                            <h3><a href="#"> اساسيات التعلم عن بعد</a></h3>
-                                            <div class="d-flex justify-content-between price">
-                                                <p class="">متبقي 2 يوم </p>
-                                                <h6>14$</h6>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+                                @endif
                             </div>
                         </div>
                     </div>
@@ -107,109 +52,44 @@
                     <div class="card">
                         <div class="widget-header">
                             <div class=" d-flex justify-content-between align-items-center">
-                                <h3 class="widget-title">الإختبارات</h3>
-                                <a href="#" class="btn btn-all"> المزيد</a>
+                                <h3 class="widget-title">الدورات التدريبة السابقة </h3>
+                                @if(count($previousCourses) > 0)
+                                <a href="{{route('instructor-courses-list', ['type' => 'past'])}}" class="btn btn-all"> المزيد</a>
+                                @endif
                             </div>
                         </div>
-                        <ul class="list-group list-group-flush mb-0">
-                            <li class="list-group-item">
-                                <div class="media align-items-center">
-                                    <div class="media-left text-light-gray ml-2">
-                                        <img src="images/exam2.png" style="width: 30px; height: 30px">
-                                    </div>
-                                    <div class="media-body">
-                                        <a class="text-body mb-1" href="fixed-#"><strong>الدورة الاولى</strong></a><br>
-                                        <div class="d-flex align-items-center">
-                                             <span class="text-blue ml-1">
-                                             <img src="images/v.png" style="width: 18px; height: 18px">
-                                             </span>
-                                            <a href="fixed-take-course.html" class="small">اساسيات</a>
+                        <div class="card-body">
+                            <div class="row">
+                                @foreach($previousCourses as $course)
+                                <div class="col-md-6 ">
+                                    <div class=" course-box">
+                                        <img src="{{asset($course->image)}}" alt="" class="img-thumbnail">
+                                        <div class="details">
+                                            <h3><a href="{{route('instructor-courses-view',['id' => $course->id, 'type' => 'current'])}}"> {{ $course->title_ar }}</a></h3>
+                                            <div class="d-flex justify-content-between price">
+                                                @if($course->type == 'live')
+                                                <h6>أونلاين</h6>
+                                                @elseif($course->type == 'recorded')
+                                                <h6>دورة مسجلة</h6>
+                                                @elseif($course->type == 'face_to_face')
+                                                <h6>حضور فعلي</h6>
+                                                @else
+                                                <h6>تعليم مدمج</h6>
+                                                @endif
+                                            </div>
                                         </div>
                                     </div>
-                                    <div class="media-right text-center d-flex align-items-center">
-                                        <h4 class="mb-0 text-warning">5.8</h4>
-                                        <span class="badge badge-warning mr-2">
-                                          جيد
-                                          </span>
+                                </div>
+                                @endforeach
+                                @if(count($previousCourses) == 0)
+                                <div class="col-md-12">
+                                    <div class="alert alert-info" style="text-align: center;">
+                                        <b>لا يوجد دورات سابقة</b>
                                     </div>
                                 </div>
-                            </li>
-                            <li class="list-group-item">
-                                <div class="media align-items-center">
-                                    <div class="media-left text-light-gray ml-2">
-                                        <img src="images/exam2.png" style="width: 30px; height: 30px">
-                                    </div>
-                                    <div class="media-body">
-                                        <a class="text-body mb-1" href="fixed-#"><strong>الدورة الثانية</strong></a><br>
-                                        <div class="d-flex align-items-center">
-                                             <span class="text-blue ml-1">
-                                             <img src="images/v.png" style="width: 18px; height: 18px">
-                                             </span>
-                                            <a href="fixed-take-course.html" class="small">اساسيات</a>
-                                        </div>
-                                    </div>
-                                    <div class="media-right text-center d-flex align-items-center">
-                                        <h4 class="mb-0 text-success">9.8</h4>
-                                        <span class="badge badge-success mr-2">
-                                          ممتاز
-                                          </span>
-                                    </div>
-                                </div>
-                            </li>
-                            <li class="list-group-item">
-                                <div class="media align-items-center">
-                                    <div class="media-left text-light-gray ml-2">
-                                        <img src="images/exam2.png" style="width: 30px; height: 30px">
-                                    </div>
-                                    <div class="media-body">
-                                        <a class="text-body mb-1" href="fixed-#"><strong>الدورة الثالثة</strong></a><br>
-                                        <div class="d-flex align-items-center">
-                                             <span class="text-blue ml-1">
-                                             <img src="images/v.png" style="width: 18px; height: 18px">
-                                             </span>
-                                            <a href="fixed-take-course.html" class="small">اساسيات</a>
-                                        </div>
-                                    </div>
-                                    <div class="media-right text-center d-flex align-items-center">
-                                        <h4 class="mb-0 text-danger">2.8</h4>
-                                        <span class="badge badge-danger mr-2">
-                                          راسب
-                                          </span>
-                                    </div>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="widget">
-                    <div class="card">
-                        <div class="widget-header">
-                            <div class=" d-flex justify-content-between align-items-center">
-                                <h3 class="widget-title">الواجبات</h3>
-                                <a href="#" class="btn btn-all"> المزيد</a>
+                                @endif
                             </div>
                         </div>
-                        <ul class="list-group list-group-flush mb-0">
-                            <li class="list-group-item">
-                                <div class="media align-items-center">
-                                    <div class="media-left text-light-gray ml-2">
-                                        <img src="images/exam2.png" style="width: 30px; height: 30px">
-                                    </div>
-                                    <div class="media-body">
-                                        <a class="text-body mb-1" href="fixed-#"><strong>أساسيات التعلم</strong></a><br>
-                                        <div class="d-flex align-items-center">
-                                             <span class="text-blue ml-1">
-                                             <img src="images/cal.png" style="width: 18px; height: 18px">
-                                             </span>
-                                            <a href="fixed-take-course.html" class="small">60 دقيقية</a>
-                                        </div>
-                                    </div>
-                                    <div class="media-right text-center d-flex align-items-center">
-                                        <a href="#" class="btn btn-all" style="width: 60px"> إبدأ</a>
-                                    </div>
-                                </div>
-                            </li>
-                        </ul>
                     </div>
                 </div>
             </div>
@@ -310,17 +190,6 @@
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-                <div class="widget ">
-                    <div class="widget-header">
-                        <div class=" d-flex justify-content-between">
-                            <h3 class="widget-title"> التقويم التدريبي </h3>
-                            <img src="images/cal.png" style="width: 25px; height: 25px">
-                        </div>
-                    </div>
-                    <div class="card cal">
-                        <div id="container" class="calendar-container"></div>
                     </div>
                 </div>
             </div>
