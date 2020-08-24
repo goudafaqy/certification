bd8de172946af117098cd01532f97ded5e6385e6
muhammad-habib <muhammad.habib.dev@gmail.com>
2020-08-24 02:11:58 +0200

    profile update

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Helpers\FileHelper;
+use App\Http\Requests\UpdateProfile;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
@@ -15,28 +16,8 @@ class ProfileController extends Controller
         return view('cp.profile.profile',compact('user'));
     }
 
-    public function save(Request $request){
-        $messages = [
-            'mobile.regex' => 'صيغه الجوال غير صحيحه مثال : 966500000000',
-            'mobile.size' => 'صيغه الجوال غير صحيحه',
-            'national_id.numeric' => 'هذا الحقل يجب ان يكون رقما',
-        ];
-
-        $validator = Validator::make($request->all(), [
-            'name_ar' => ['required', 'string', 'max:255'],
-            'name_en' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', Rule::unique('users')->ignore($user->id)],
-            'national_id' => ['required', 'numeric', Rule::unique('users')->ignore($user->id)],
-            'mobile' => 'required|regex:/(966)[0-9]{9}/|size:12',
-            'birth_date' => ['required', 'date'],
-            'gender' => ['required'],
-            'education' => ['required']
-        ], $messages);
-        if ($validator->fails()){
-            return redirect()->back()
-                ->withErrors($validator)
-                ->withInput();
-        }
+    public function save(UpdateProfile $request){
+        $user = Auth::user();
         $user->fill($request->all());
         if($request->file()) {
             $filePath = FileHelper::uploadFiles($request->file('image'), 'uploads/profiles/');
