674aeab65ccd12b90c5ba9d38b962b75d9bea7ca
AymanElbery <ayman.bery@gmail.com>
2020-08-24 18:12:13 +0300

    refactoring courseAppointmentsRepo

--- app/Http/Repositories/Eloquent/CourseAppointmentRepo.php
+++ app/Http/Repositories/Eloquent/CourseAppointmentRepo.php
@@ -4,42 +4,13 @@ namespace App\Http\Repositories\Eloquent;
 
 use App\Http\Interfaces\Eloquent\CourseAppointmentEloquent;
 use App\Models\CourseAppintment;
-
-class CourseAppointmentRepo implements CourseAppointmentEloquent{
+class CourseAppointmentRepo extends Repository implements CourseAppointmentEloquent{
     
-    public function getAll()
+    public function __construct()
     {
-        return CourseAppintment::all()->sortBy('date');
+        parent::__construct(new CourseAppintment());
     }
-
-
-    public function getById($id)
-    {
-        return CourseAppintment::find($id);
-    }
-
-
-    public function save($inputs, $getId = false)
-    {
-        return ($getId) ? CourseAppintment::insertGetId($inputs) : CourseAppintment::create($inputs) ;
-    }
-
-    public function saveBulk($inputs)
-    {
-        return CourseAppintment::insert($inputs) ;
-    }
-
-    public function update($inputs, $id)
-    {
-        return CourseAppintment::where('id', $id)->update($inputs);
-    }
-
-
-    public function delete($id)
-    {
-        return CourseAppintment::where('id', $id)->delete();
-    }
-
+    
     public function deleteByCourseId($course_id)
     {
         return CourseAppintment::where('course_id', $course_id)->delete();
