ecdc675f5673be79a43958279de23185402e625e
AymanElbery <ayman.bery@gmail.com>
2020-08-24 18:28:46 +0300

    refactoring unitRepo

--- app/Http/Repositories/Eloquent/UnitRepo.php
+++ app/Http/Repositories/Eloquent/UnitRepo.php
@@ -5,34 +5,16 @@ namespace App\Http\Repositories\Eloquent;
 use App\Http\Interfaces\Eloquent\UnitEloquent;
 use App\Models\Unit;
 
-class UnitRepo implements UnitEloquent{
-    
+class UnitRepo extends Repository implements UnitEloquent{
+
+    public function __construct()
+    {
+        parent::__construct(new Unit());
+    }
+
     public function getAll($section_id = '')
     {
         return Unit::where('section_id',$section_id)->get();
-    }
-
-
-    public function getById($id)
-    {
-        return Unit::where('id', $id)->first();
-    }
-
-
-    public function save($inputs, $getId = false)
-    {
-        return ($getId) ? Unit::insertGetId($inputs) : Unit::create($inputs);
-    }
-
-    public function update($inputs, $id)
-    {
-        return Unit::where('id', $id)->update($inputs);
-    }
-
-
-    public function delete($id)
-    {
-        return Unit::where('id', $id)->delete();
     }
 
 }
