dc37b7ba8d743611504c1a16d018a7bbc8dba443
Muhammed AboElgoud <mohgood2020@gmail.com>
2020-09-01 20:17:58 +0200

    update certificates

--- app/Http/Controllers/CertificatesController.php
+++ app/Http/Controllers/CertificatesController.php
@@ -7,7 +7,6 @@ use Illuminate\Http\Request;
 use Intervention\Image\Facades\Image;
 use App\Http\Helpers\FileHelper;
 use Illuminate\Support\Facades\Mail;
-use App\Models\Advertisment;
 use App\Models\Newsletter;
 use Johntaa\Arabic\I18N_Arabic;
 use App\Mail\SendEmail;
@@ -38,38 +37,118 @@ class CertificatesController extends Controller
     public function certificates()
     {
 
-        $data = ['title_ar' => 'Sending Multiple Emails','message_ar'=>'Sending Multiple Emails','link'=>'lin','extra_text'=>''];
-        $email = new SendEmail($data , __('app.Adly Training Center') ,$data['title_ar'] ,  'Generic');
-        $evenMyMoreUsers = ['mohgood2020@gmail.com','devmogoud@gmail.com'];
-        Mail::to([])
-        ->bcc($evenMyMoreUsers)
-        ->send($email);
-        $Arabic = new I18N_Arabic('Glyphs'); 
-        $textAR ="محمد محمود ابوالجود";
-        $textEn ="Muhammed  AboElgoud";
-        $textAR = $Arabic->utf8Glyphs($textAR); 
-        $img = Image::make(public_path('images/certificate.jpeg')); 
-        $img->text($textAR, 910, 280, function($font) {  
+        // $data = ['title_ar' => 'Sending Multiple Emails','message_ar'=>'Sending Multiple Emails','link'=>'lin','extra_text'=>''];
+        // $email = new SendEmail($data , __('app.Adly Training Center') ,$data['title_ar'] ,  'Generic');
+        // $evenMyMoreUsers = ['mohgood2020@gmail.com','devmogoud@gmail.com'];
+        // Mail::to([])
+        // ->bcc($evenMyMoreUsers)
+		// ->send($email);
+		// die('Sending');
+		$data =['name_ar'=>'   محمد محمد محمود ابوالجود',
+				 'name_en'=>'',
+				 'national_id'=>'19872200552',
+				 'course_ar'=>'أساسيات وسائل التواصل الاجتماعي',
+				 'course_en'=>'',
+				 'hours'=>'65',
+				 'date'=>date('Y-M-d')];
+		$this->generate($data);
+        
+    }
+
+
+	 /**
+     * Generate Full Certificates
+     */
+    public function generate($data)
+    {
+		
+		$Arabic = new I18N_Arabic('Glyphs'); 
+       
+		$nameAR = $Arabic->utf8Glyphs($data['name_ar']); 
+		$courseAR = $Arabic->utf8Glyphs($data['course_ar']); 
+		$img = Image::make(public_path('images/certificate.jpeg')); 
+		$hourse = $Arabic->utf8Glyphs('ساعة '); 
+		// Name
+
+        $img->text($nameAR, 880, 280, function($font) {  
            $font->file(public_path('fonts/ae_AlHor.ttf'));  
-           $font->size(25);  
+           $font->size(28);  
            $font->color('#000000');  
            $font->align('center');  
            $font->valign('bottom');  
            $font->angle(0);  
        });  
 
-       $img->text($textEn, 320, 280, function($font) {  
-        $font->file(public_path('fonts/ae_AlHor.ttf'));  
-        $font->size(25);  
-        $font->color('#000000');  
-        $font->align('center');  
-        $font->valign('bottom');  
-        $font->angle(0);  
-    });  
+       $img->text($data['name_en'], 270, 280, function($font) {  
+			$font->file(public_path('fonts/arial.ttf'));  
+			$font->size(20);  
+			$font->color('#000000');  
+			$font->align('center');  
+			$font->valign('bottom');  
+			$font->angle(0);  
+	   });  
 
-       $img->save(public_path('images/hardik3.jpg'));  
+
+	  // ID 
+
+	  $img->text($data['national_id'], 850, 320, function($font) {  
+		$font->file(public_path('fonts/arial.ttf'));  
+		$font->size(25);  
+		$font->color('#000000');  
+		$font->align('center');  
+		$font->valign('bottom');  
+		$font->angle(0);  
+	  });  
+
+	   // Course 
+
+	   
+	   $img->text($courseAR, 765, 375, function($font) {  
+		$font->file(public_path('fonts/ae_AlHor.ttf'));  
+		$font->size(27);  
+		$font->color('#000000');  
+		$font->align('center');  
+		$font->valign('bottom');  
+		$font->angle(0);  
+	  });  
+	  
+	  $img->text($data['course_en'], 500, 363, function($font) {  
+		$font->file(public_path('fonts/arial.ttf'));  
+		$font->size(20);  
+		$font->color('#000000');  
+		$font->align('center');  
+		$font->valign('bottom');  
+		$font->angle(0);  
+	  });  
+	  
+
+	  // Date 
+
+	  $img->text($data['date'], 850, 425, function($font) {  
+		$font->file(public_path('fonts/arial.ttf'));  
+		$font->size(22);  
+		$font->color('#000000');  
+		$font->align('center');  
+		$font->valign('bottom');  
+		$font->angle(0);  
+	  });  
+
+
+
+	  // Hours 
+
+	  $img->text($hourse.' '.$data['hours'], 840, 460, function($font) {  
+		$font->file(public_path('fonts/ae_AlHor.ttf'));  
+		$font->size(22);  
+		$font->color('#000000');  
+		$font->align('center');  
+		$font->valign('bottom');  
+		$font->angle(0);  
+	  });  
+	  $name = "certifcate".time();
+	  $img->save(public_path('certifcates/'.$name.'.jpg'));  
+	   
     }
-
 
     /**
      * Get add classification page ...
--- config/app.php
+++ config/app.php
@@ -161,8 +161,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Jorenvh\Share\Providers\ShareServiceProvider::class,
-
         /*
          * Package Service Providers...
          */
@@ -232,14 +230,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-<<<<<<< HEAD
-        'Image' => Intervention\Image\Facades\Image::class
-
-
-
-=======
-        'Share' => Jorenvh\Share\ShareFacade::class,
->>>>>>> dev
+        'Image' => Intervention\Image\Facades\Image::class,
+        
 
     ],
 
