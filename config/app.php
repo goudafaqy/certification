9316398cd176e6a658bc55bbf9f44bda8bebe57f
AymanElbery <ayman.bery@gmail.com>
2020-09-09 10:58:28 +0300

    merge

--- config/app.php
+++ config/app.php
@@ -161,7 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Jorenvh\Share\Providers\ShareServiceProvider::class,
+        //Jorenvh\Share\Providers\ShareServiceProvider::class,
         Alkoumi\LaravelHijriDate\LaravelHijriDateServiceProvider::class,
 
         /*
