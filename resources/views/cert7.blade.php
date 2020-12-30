f65ca0d9828cfd70505a393bdd4fc1537b12f0d3
MohammedGoud <mohgood2020@gmail.com>
2020-12-30 13:18:03 +0200

    fff

--- resources/views/cert7.blade.php
+++ resources/views/cert7.blade.php
@@ -265,7 +265,7 @@
                                             
                                             <h4 style="font-family:AL-Mohanad;font-size:25px">د.فارس بن محمد القرني</h4>
                                             <h4 style="font-family:AL-Mohanad;font-size:20px">Dr. Faris M. Algarni</h4>
-                                            <div class="qrcode" style="text-align:left"> {!! QrCode::color(28,97,20)->size(100)->generate(route('verificationCertificate',['national_id'=>$data['n_id'],'course' => $data['id'] ])); !!}</div>
+                                            <div class="qrcode" style="text-align:left;margin-bottom:100px"> {!! QrCode::color(28,97,20)->size(100)->generate(route('verificationCertificate',['national_id'=>$data['n_id'],'course' => $data['id'] ])); !!}</div>
 
                                         </div>
                                         
