2edea77737987fb24ebb38b66841ef9fe35d0016
MohammedGoud <mohgood2020@gmail.com>
2020-12-30 13:06:04 +0200

    fff

--- resources/views/cert7.blade.php
+++ resources/views/cert7.blade.php
@@ -195,7 +195,7 @@
                     <div class=" col-md-12 p-0 d-md-block d-lg-block">
                         <div class="bg2">
                             <div class="overlay"></div>
-                            <div class="col-xl-12 col-lg-12 col-md-12 my-auto" style="display: table;overflow: hidden;">
+                            <div class="col-xl-12 col-lg-12 col-md-12 my-auto" style="margin: auto;display: table;overflow: hidden;">
                             <div class="certificado_conteudo ">
                                 <img src="{{asset('site-assets/images/certificate/background.jpg') }}" style="box-shadow: 1px 1px 5px 0px rgba(82, 82, 82, 0.75)">
                             </div>
@@ -266,7 +266,6 @@
                                             
                                             <h4 style="font-family:AL-Mohanad;font-size:25px">د.فارس بن محمد القرني</h4>
                                             <h4 style="font-family:AL-Mohanad;font-size:20px">Dr. Faris M. Algarni</h4>
-                                            <div class="qrcode" style="text-align:left"> {!! QrCode::color(28,97,20)->size(100)->generate(route('verificationCertificate',['national_id'=>$data['n_id'],'course' => $data['id'] ])); !!}</div>
 
                                         </div>
                                         
